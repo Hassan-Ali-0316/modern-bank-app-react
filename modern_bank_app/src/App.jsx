71260346fa52e25905607eaec7c93e525a13b276
Hassan <ha159193@gmail.com>
2024-10-06 01:51:29 +0500

    added files

---
+++ modern_bank_app/src/App.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import styles from './style'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Business from './components/Business'
+import Footer from './components/Footer'
+import Stats from './components/Stats'
+import Clients from './components/Clients'
+import CTA from './components/CTA'
+import CardDeal from './components/CardDeal'
+import Billing from './components/Billing'
+import Testimonials from './components/Testimonials'
+
+
+const App = () => {
+  return (
+    <div className='bg-primary w-full overflow-hidden'>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+            <Navbar/>
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero/>
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart} ${styles.paddingX}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats/>
+          <Business/>
+          <CardDeal/>
+          <Testimonials/>
+          <Clients/>
+          <CTA/>
+          <Footer/>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
